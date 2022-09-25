69e8a7177ee2aa161bec1758dbb42d0eae455edf
hamzeNasirii <hamze_nasirii@yahoo.com>
2022-09-25 14:03:34 +0330

    import jalali-date app

--- eCRF/admin.py
+++ eCRF/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import DemoInfo, PatientHistory
+from jalali_date.admin import ModelAdminJalaliMixin
 
 
 @admin.register(DemoInfo)
-class DemoAdmin(admin.ModelAdmin):
+class DemoAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
     list_display = ['d_national_code', 'd_first_name', 'd_last_name', 'd_birthday', 'd_gender', 'datetime_created',
                     'datetime_modified']
 
